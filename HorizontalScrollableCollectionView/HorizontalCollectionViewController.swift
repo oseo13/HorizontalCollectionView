7d46d13a587869e70328f293ba1e5aa7e9ed59e0
young <tispf1994@naver.com>
2020-01-11 16:51:30 +0900

    first commit

---
+++ HorizontalScrollableCollectionView/HorizontalCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HorizontalCollectionViewController.swift
+//  HorizontalScrollableCollectionView
+//
+//  Created by youngseo on 2020/01/11.
+//  Copyright © 2020 youngseo. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HorizontalScrollableCollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  HorizontalScrollableCollectionView
+//
+//  Created by youngseo on 2020/01/11.
+//  Copyright © 2020 youngseo. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
