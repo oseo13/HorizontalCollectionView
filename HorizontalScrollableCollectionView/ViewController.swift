10ada8ee065dde90481ec074a32cd3f06959e24a
young <tispf1994@naver.com>
2020-01-11 15:19:37 +0900

    Initial Commit

---
+++ HorizontalScrollableCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HorizontalScrollableCollectionView
+//
+//  Created by youngseo on 2020/01/11.
+//  Copyright © 2020 youngseo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
